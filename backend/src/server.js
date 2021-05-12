45ba6270251e6d0575cb31e62ad7da09899c8d7f
henrickJona <jhenrick41@gmail.com>
2021-05-12 13:14:19 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,29 @@ require('dotenv').config()
 /* import express from 'express'
 import routes from './routes' */
 const routes = require("./routes");
+const Sequelize = require("sequelize");
+const dbConfig = require("../src/config/database");
 /* import 'dotenv/config'; */
 /* import database from './database' */
-require("./database/index");
+/* require("./database/index"); */
 const cors = require('cors')
+const Users = require("../src/app/models/Users");
+const Events = require("../src/app/models/Events");
+const Buildings = require("../src/app/models/Buildings");
+const Categories = require("../src/app/models/Categories");
 
+const connection = new Sequelize(process.env.DATABASE_URL,{ dbConfig});
+
+connection.authenticate().then(()=>console.log('Connection has been established successfully.',)).catch((error)=>console.log('Unable to connect to the database:', error))
+    
+ 
+Users.init(connection);
+
+Buildings.init(connection);
+Categories.init(connection);
+Events.init(connection);
+Buildings.associate(connection.models);
+Events.associate(connection.models);
 const app = express();
 
 app.use(cors())
