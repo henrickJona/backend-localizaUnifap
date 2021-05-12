c4f40b946553f9f09497b78475492d326296cf97
henrickJona <jhenrick41@gmail.com>
2021-05-12 12:25:59 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,6 +13,6 @@ app.use(cors())
 app.use(express.json());
 app.use(routes);
 
-/* app.listen(process.env.PORT ||5000); */
-app.listen(8080); 
+app.listen(process.env.PORT ||5000);
+/* app.listen(8080);  */
 /* export default server; */
