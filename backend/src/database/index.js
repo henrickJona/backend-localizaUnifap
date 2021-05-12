541cc25d40582af244803167d8ac9b0585c27139
henrickJona <jhenrick41@gmail.com>
2021-05-12 14:37:13 -0300

    Update index.js

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -6,9 +6,10 @@ const Events = require("../app/models/Events");
 const Buildings = require("../app/models/Buildings");
 const Categories = require("../app/models/Categories");
 
-const connection = new Sequelize(process.env.DATABASE_URL/* ,{ dbConfig} */);
+const connection = new Sequelize(process.env.DATABASE_URL,{ dbConfig});
 console.log('comecou')
-connection.authenticate().then(()=>console.log('Connection has been established successfully.',)).catch((error)=>console.log('Unable to connect to the database:', error))
+connection.authenticate().then(()=>console.log('Connection has been established successfully.',))
+.catch((error)=>console.log('Unable to connect to the database:', error))
     
 console.log('passou')
 Users.init(connection);
