ec394febad0bc480b9805fa632ef4805cb6cb175
henrickJona <jhenrick41@gmail.com>
2021-05-12 12:23:31 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ import routes from './routes' */
 const routes = require("./routes");
 /* import 'dotenv/config'; */
 /* import database from './database' */
-require("./database");
+require("./database/index");
 const cors = require('cors')
 
 const app = express();
@@ -13,6 +13,6 @@ app.use(cors())
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT ||5000);
-/* app.listen(8080);  */
+/* app.listen(process.env.PORT ||5000); */
+app.listen(8080); 
 /* export default server; */
